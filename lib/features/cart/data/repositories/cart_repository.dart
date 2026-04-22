ebca380332230e897682140ac7a8cc81e1d83d7d
hartamatamatama <katonojutsu340@gmail.com>
2026-04-22 15:57:08 +0700

    feat(cart): enhance repository with shipping address and secure token storage
    Memperbarui CartRepository untuk menggunakan SecureStorageService dalam mengambil token backend dan menambahkan field shipping_address ke payload checkout untuk memenuhi validasi backend Golang.

--- lib/features/cart/data/repositories/cart_repository.dart
+++ lib/features/cart/data/repositories/cart_repository.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../../../../core/services/secure_storage.dart'; // Import brankas tokenmu
+import '../../../../core/services/secure_storage.dart';
 import '../models/cart_item_model.dart';
 
 class CartRepository {
@@ -8,7 +8,7 @@ class CartRepository {
 
   Future<bool> checkout(List<CartItemModel> items, double totalAmount) async {
     try {
-      // 1. Ambil ID Card Golang dari Brankas (Secure Storage)
+      // 1. Ambil ID Card Golang dari Brankas
       final token = await SecureStorageService.getToken();
 
       if (token == null) {
@@ -16,7 +16,7 @@ class CartRepository {
         return false;
       }
 
-      // 2. Siapkan data paket JSON
+      // 2. Siapkan data paket JSON beserta alamat pengiriman
       final body = jsonEncode({
         "items": items
             .map(
@@ -29,20 +29,22 @@ class CartRepository {
             .toList(),
         "total_amount": totalAmount,
         "order_date": DateTime.now().toIso8601String(),
+        "shipping_address":
+            "Jl. Merdeka No. 1, Tangerang", // Alamat sementara agar lolos validasi
       });
 
-      // 3. Tembak API dengan Token Backend yang benar
+      // 3. Tembak API
       final response = await http.post(
         Uri.parse("$baseUrl/orders/checkout"),
         headers: {
           "Content-Type": "application/json",
-          "Authorization":
-              "Bearer $token", // Kirim token Golang ke Satpam Golang
+          "Authorization": "Bearer $token",
         },
         body: body,
       );
 
       print("Status Code Golang: ${response.statusCode}");
+      print("Balasan Golang: ${response.body}");
 
       return response.statusCode == 201 || response.statusCode == 200;
     } catch (e) {
